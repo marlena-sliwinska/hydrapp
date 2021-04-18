ff3d71c257b4fcf55c173d0858e6f12c135667ee
Marlena Śliwińska <marlenasliwinska@gmail.com>
2021-04-18 08:24:56 +0200

    local storage added

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,41 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
-import {registerSW} from './pwa.js';
+import { registerSW } from "./pwa.js";
 registerSW();
 
 /* place your code below */
+// import moment from 'moment';
 
-console.log('HELLO 🚀');
+console.log("HELLO 🚀");
 
-let counter = document.querySelector('.hydrapp__counter');
+const counter = document.querySelector(".hydrapp__counter");
+const addButton = document.querySelector(".hydrapp__addbutton--js");
 
-let n=0;
-console.log(n);
+const removeButton = document.querySelector(".hydrapp__removebutton--js");
+const key = new Date().toLocaleString().slice(0, 10);
 
-function addGlass(i)
-{  
-    n=n+1;
-    counter.innerHTML=n; 
+let n = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+  n = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+counter.innerHTML = localStorage.getItem(key);
+function addGlass(i) {
+  n++;
+  counter.innerHTML = n;
+  localStorage.setItem(key, n);
 }
 
+function removeGlass(i) {
+  if (n > 0) n--;
+  counter.innerHTML = n;
+  localStorage.setItem(key, n);
+}
 
-function removeGlass(i)
-{ 
-    if (n>0) n=n-1;
-    counter.innerHTML=n; 
-  }
-
-
-const addButton = document.querySelector('.hydrapp__addbutton--js');
-addButton.addEventListener('click',addGlass);
-
-const removeButton = document.querySelector('.hydrapp__removebutton--js');
-removeButton.addEventListener('click',removeGlass);
+addButton.addEventListener("click", addGlass);
+removeButton.addEventListener("click", removeGlass);
