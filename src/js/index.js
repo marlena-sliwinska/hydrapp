96235ce4a076a753b076c7a3e9c54df842322f54
Marlena Śliwińska <marlenasliwinska@gmail.com>
2021-04-18 07:47:47 +0200

    counter made

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,29 @@ registerSW();
 
 /* place your code below */
 
-console.log('HELLO 🚀')
+console.log('HELLO 🚀');
+
+let counter = document.querySelector('.hydrapp__counter');
+
+let n=0;
+console.log(n);
+
+function addGlass(i)
+{  
+    n=n+1;
+    counter.innerHTML=n; 
+}
+
+
+function removeGlass(i)
+{ 
+    if (n>0) n=n-1;
+    counter.innerHTML=n; 
+  }
+
+
+const addButton = document.querySelector('.hydrapp__addbutton--js');
+addButton.addEventListener('click',addGlass);
+
+const removeButton = document.querySelector('.hydrapp__removebutton--js');
+removeButton.addEventListener('click',removeGlass);
